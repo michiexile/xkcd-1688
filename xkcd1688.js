5c6b165dbba9e9b07e0c9ff6394782845583e8c2
Mikael Vejdemo-Johansson <michiexile@gmail.com>
2016-06-03 11:40:48 +0200

    debugging...

--- xkcd1688.js
+++ xkcd1688.js
@@ -1,9 +1,11 @@
 // developed against jQuery 1.12.2 and d3.js 
 
+var xkcd_data;
 $(document).ready(function(){
     d3.xml("xkcd1688.xml", function(error, xkcd1688) {
 	if(error) throw error;
-
+	xkcd_data = xkcd1688;
+/*	
 	var vertices = [].map.call(
 	    xkcd1688.querySelectorAll("node, leaf"),
 	    function(node) {
@@ -15,7 +17,7 @@ $(document).ready(function(){
 			})
 		};
 	    });
-	
+*/	
 	var canvas = $("#canvas").get(0);
 	var ctx = canvas.getContext("2d");
 	
