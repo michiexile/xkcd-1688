51e09d60ceba4c121d63e66ef92794c2658b7c09
Mikael Vejdemo-Johansson <michiexile@gmail.com>
2016-06-03 11:50:32 +0200

    debugging...

--- xkcd1688.js
+++ xkcd1688.js
@@ -1,25 +1,23 @@
 // developed against jQuery 1.12.2 and d3.js 
 
 var xkcd_data;
-$(document).ready(function(){
-    d3.xml("xkcd1688.xml", function(error, xkcd1688) {
-	if(error) throw error;
-	xkcd_data = xkcd1688;
-	
-	var vertices = [].map.call(
-	    xkcd1688.querySelectorAll("node, leaf"),
-	    function(node) {
-		return {
-		    id: node.getAttribute("id"),
-		    children: node.querySelectorAll("option").map.call(
-			function(option) {
-			    return option.getAttribute("target");
-			})
-		};
-	    });
-	
-	var canvas = $("#canvas").get(0);
-	var ctx = canvas.getContext("2d");
-	
-    });
+d3.xml("xkcd1688.xml", function(error, xkcd1688) {
+    if(error) throw error;
+    xkcd_data = xkcd1688;
+    
+    var vertices = [].map.call(
+	xkcd1688.querySelectorAll("node, leaf"),
+	function(node) {
+	    return {
+		id: node.getAttribute("id"),
+		children: node.querySelectorAll("option").map.call(
+		    function(option) {
+			return option.getAttribute("target");
+		    })
+	    };
+	});
+    
+    var canvas = $("#canvas").get(0);
+    var ctx = canvas.getContext("2d");
+    
 });
