5d0c8d69f53fc213e163dd2d78b6e0640e6b7505
Mikael Vejdemo-Johansson <michiexile@gmail.com>
2016-06-05 23:33:11 +0200

    all but fictional/non-map branch

--- xkcd1688.js
+++ xkcd1688.js
@@ -3,123 +3,122 @@
 var xkcd1688;
 
 var width=600,
-    height=600,
-    linewidth=40;
+    height=50;
 
 var svg = d3.select("#svg_canvas").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-var force = d3.layout.force()
-    .charge(-120)
-    .linkDistance(30)
-    .size([width, height]);
+var gradient = svg.append('defs')
+    .attr('id', 'axis_gradient')
+    .append('linearGradient');
+gradient.append('stop')
+    .attr('offset', '0%')
+    .attr('stop-color', 'blue')
+    .attr('stop-opacity', '0.0');
+gradient.append('stop')
+    .attr('offset', '95%')
+    .attr('stop-color', 'black')
+    .attr('stop-opacity', '1.0');
 
-function draw_timeline(cx, cy, w, ranges) {
+
+var axis = svg.append('g');
+var active = svg.append('g')
+    .attr('id', 'active');
+
+var year_scale = d3.scale.linear()
+    .domain([1790, 2030])
+    .range([0, width]);
+
+axis.append('line')
+    .attr('x1', year_scale(1800))
+    .attr('x2', year_scale(1805))
+    .attr('y1', height/2)
+    .attr('y2', height/2)
+    .attr('stroke-width', '2')
+    .attr('class', 'intro');
+axis.append('line')
+    .attr('x1', year_scale(1805))
+    .attr('x2', year_scale(2025))
+    .attr('y1', height/2)
+    .attr('y2', height/2)
+    .attr('stroke-width', '2')
+    .attr('class', 'timeline');
+for(var i=0; i<(203-180); i++) {
+    var offset = 5;
+    if((i % 5) == 0)
+	offset = 7;
+    if((i % 10) == 0)
+	offset = 10;
     
+    axis.append('line')
+	.attr('x1', year_scale(1800+10*i))
+	.attr('x2', year_scale(1800+10*i))
+	.attr('y1', height/2-offset)
+	.attr('y2', height/2+offset)
+	.attr('stroke-width', '1')
+	.attr('class', 'timeline');
 }
+axis.append('text')
+    .attr('x', year_scale('1800'))
+    .attr('y', height/2+20)
+    .attr('text-anchor', 'middle')
+    .text('1800');
+axis.append('text')
+    .attr('x', year_scale('1900'))
+    .attr('y', height/2+20)
+    .attr('text-anchor', 'middle')
+    .text('1900');
+axis.append('text')
+    .attr('x', year_scale('2000'))
+    .attr('y', height/2+20)
+    .attr('text-anchor', 'middle')
+    .text('2000');
+
+function ask_question(xkcd1688, node) {
+    $('#question').text(node.find("question").first().text().trim());
+
+    console.log(node.find('option'));
+    var divs = d3.select('#options')
+	.selectAll('div')
+	.data(node.find('option').toArray());
+    divs.exit().remove();
+    divs.enter()
+    	.append('div');
+    
+    divs.attr('id', function(d) {
+	    console.log(d);
+	    return $(d).attr('target');})
+	.on('click', function(d) {
+	    ask_question(xkcd1688,
+			 $(xkcd1688).find('#'+$(d).attr('target')).first());
+	})
+	.text(function(d) { return $(d).text().trim(); });
+
+    $('.active').remove();
+    var begin = parseFloat(node.find('range > begin').attr('year'));
+    if(isNaN(begin)) begin = 1800;
+    var end = parseFloat(node.find('range > end').attr('year'));
+    if(isNaN(end)) end = 2030;
+    var begin = Math.max(1800, begin);
+    var end = Math.min(2025, end);
+    $('#range').text(begin + " " + end);
+    active.append('line')
+	.attr('x1', year_scale(begin))
+	.attr('x2', year_scale(end))
+	.attr('y1', height/2)
+	.attr('y2', height/2)
+	.attr('stroke-width', '4')
+	.attr('class', 'active');
+}
+
 
 d3.xml("xkcd1688.xml", function(error, data) {
     if(error) throw error;
     xkcd1688 = data;
 
-    var vertices = $.map(
-	$(xkcd1688).find("node, leaf"),
-	function(node) {
-	    return {
-		id: node.id,
-		children: $(node).find("option"),
-		level: 0,
-		question: $.trim($($(node).find("question").get(0)).text()),
-		from: $($(node).find("range > begin").get(0)).attr("year"),
-		to: $($(node).find("range > end").get(0)).attr("year")
-	    }
-	});
-    
-    var vertices_ids = $.map(vertices, function(vtx){ return vtx.id; });
-    var edges = [];
-    var maxlevel = 0;
-    for(var i=0; i<vertices.length; i++) {
-	var node = vertices[i];
-	for(var j=0; j<node.children.length; j++) {
-	    var child = node.children[j];
-	    var child_id = $.inArray($(child).attr('target'), vertices_ids);
-	    if(child_id > 0) {
-		edges.push({
-		    source: i,
-		    target: child_id,
-		    option: $(child).text()
-		});
-	    }
-	    if((vertices[child_id] != undefined) &&
-	       (vertices[child_id].level == 0)) {
-		vertices[child_id].level = node.level + 1;
-		if(maxlevel < vertices[child_id].level) {
-		    maxlevel = vertices[child_id].level;
-		}
-	    }
-	}
-    }
+    var node = $(xkcd1688).find("#root").first();
 
-    var steps = height / (maxlevel + 2);
-    
-    force
-	.nodes(vertices)
-        .links(edges)
-        .start();
-
-    var links = svg.selectAll(".link")
-	.data(edges)
-	.enter()
-	.append("line")
-	.classed("link", true);
-    
-    var nodes = svg.selectAll(".node")
-	.data(vertices)
-	.enter()
-	.append("svg")
-	.attr("height", 10)
-	.attr("width", linewidth)
-	.classed("node", true)
-	.call(force.drag);
-
-    nodes.append("title")
-	.text(function(d) { return d.question; });
-
-    nodes.append("rect")
-	.attr("x", 0)
-	.attr("y", 0)
-	.attr("height", 1)
-	.attr("width", linewidth)
-	.classed("timeline-backdrop", true);
-    nodes.append("rect")
-	.attr("x", function(d) {
-	    if(d.from != undefined) {
-		return parseFloat(d.from);
-	    } else {
-		return 0;
-	    }
-	})
-	.attr("width", function(d) {
-	    if((d.from != undefined) && (d.to != undefined)) {
-		return parseFloat(d.to)-parseFloat(d.from);
-	    } else {
-		return 0;
-	    }
-	})
-	.attr("y", 0)
-	.attr("height", 5)
-	.classed("timeline-active", true);
-    
-    force.on("tick", function() {
-	nodes.attr("x", function(d) { return d.x; })
-	    .attr("y", function(d) {
-		d.y = steps * (d.level + 1);
-		return d.y; });
-	links.attr("x1", function(d) { return d.source.x; })
-	    .attr("y1", function(d) { return d.source.y; })
-	     .attr("x2", function(d) { return d.target.x; })
-	     .attr("y2", function(d) { return d.target.y; });
-    });
+    ask_question(xkcd1688, node);
 });
  
